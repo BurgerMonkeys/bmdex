043421999b3083e56fa78a49242c4cf249c9c2e0
Ricardo Prestes <ricardo.logan@hotmail.com>
2021-04-19 20:41:56 -0300

    Create project structure

---
+++ src/BMDex.Test/Test.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+namespace BMDex.Test
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+        }
+    }
+}
