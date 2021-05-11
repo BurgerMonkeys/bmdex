9a0905ac03bd584ae00d64e5b87069b477d1db98
Breno Angelotti <brenogangelotti@gmail.com>
2021-05-10 22:43:56 -0300

    get pokemon with offset or by id

--- src/BMDex/Services/PokemonService.cs
+++ src/BMDex/Services/PokemonService.cs
@@ -6,7 +6,8 @@ namespace BMDex.Services
 {
     public interface IPokemonService
     {
-        public Task<IEnumerable<Pokemon>> GetPokemon();
+        public Task<IEnumerable<Pokemon>> GetPokemon(int limit, int offset);
+        public Task<Pokemon> GetPokemon(int id);
     }
 
     public class PokemonService : IPokemonService
@@ -18,12 +19,27 @@ namespace BMDex.Services
             _pokeApiClient = new PokeApiClient();
         }
 
-        public async Task<IEnumerable<Pokemon>> GetPokemon()
+        public async Task<IEnumerable<Pokemon>> GetPokemon(int limit = 20, int offset = 0)
         {
-            var content = await _pokeApiClient.GetNamedResourcePageAsync<Pokemon>(20, 0);
+            var content = await _pokeApiClient.GetNamedResourcePageAsync<Pokemon>(limit, offset);
             var pokemonData = await _pokeApiClient.GetResourceAsync(content.Results);
 
             return pokemonData;
+        }
+
+        public async Task<Pokemon> GetPokemon(int id)
+        {
+            if (id <= 0) return null;
+            try
+            {
+                var pokemon = await _pokeApiClient.GetResourceAsync<Pokemon>(id);
+                return pokemon;                
+            }
+            catch (System.Exception ex)
+            {
+                System.Console.WriteLine(ex.Message);
+                return null;
+            }
         }
     }
 }
