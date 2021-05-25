b7b99181ea9f17e833782d85cbf0a5b8f2a94956
Eduardo <eduardo@forlogic.net>
2021-05-24 21:18:49 -0300

    add ability service and test

---
+++ src/BMDex.Tests/Services/AbilityServiceTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+using BMDex.Services;
+using FluentAssertions;
+using Xunit;
+
+namespace BMDex.Tests.Services
+{
+    public class AbilityServiceTest
+    {
+        IAbilityService _abilityService;
+
+        public AbilityServiceTest()
+        {
+            _abilityService = new AbilityService();
+        }
+
+        [Theory]
+        [InlineData(20, 0, 20)]
+        [InlineData(100, 300, 27)]
+        [InlineData(100, 400, 0)]
+        [InlineData(100, -10, 0)]
+        [InlineData(-11, -10, 0)]
+        public async Task GetAbilities(int limit, int offset, int expected)
+        {
+            var result = await _abilityService.GetAbilities(limit, offset);
+
+            result.Should().NotBeNull();
+            result.Count.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData(1, "stench")]
+        [InlineData(100, "stall")]
+        [InlineData(253, "perish-body")]
+        public async Task GetAbilityByValidId(int id, string expected)
+        {
+            var result = await _abilityService.GetAbilityById(id);
+
+            result.Should().NotBeNull();
+            result.Name.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        [InlineData(int.MaxValue)]
+        public async Task GetAbilityByInvalidId(int id)
+        {
+            var result = await _abilityService.GetAbilityById(id);
+            result.Should().BeNull();
+        }
+    }
+}
---
+++ src/BMDex/Services/AbilityService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using PokeApiNet;
+
+namespace BMDex.Services
+{
+    public interface IAbilityService
+    {
+        Task<List<Ability>> GetAbilities(int limit, int offset);
+        Task<Ability> GetAbilityById(int id);
+    }
+
+    public class AbilityService : IAbilityService
+    {
+        readonly PokeApiClient _pokeApiClient;
+
+        public AbilityService()
+        {
+            _pokeApiClient = new PokeApiClient();
+        }
+
+        public async Task<List<Ability>> GetAbilities(int limit, int offset)
+        {
+            var content = await _pokeApiClient.GetNamedResourcePageAsync<Ability>(limit, offset);
+            var abilityData = await _pokeApiClient.GetResourceAsync(content.Results);
+
+            return abilityData;
+        }
+
+        public async Task<Ability> GetAbilityById(int id)
+        {
+            if (id <= 0)
+                return null;
+
+            try
+            {
+                var ability = await _pokeApiClient.GetResourceAsync<Ability>(id);
+                return ability;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+    }
+}
