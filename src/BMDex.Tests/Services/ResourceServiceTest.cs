c517a23e9fba9faa610d2b80337f8c5afa408740
Breno Angelotti <brenogangelotti@gmail.com>
2021-08-11 22:28:18 -0300

    ResourceService tests

--- src/BMDex.Tests/Services/ResourceServiceTest.cs
+++ src/BMDex.Tests/Services/ResourceServiceTest.cs
@@ -1,11 +1,12 @@
-﻿using System.Net.Http;
+﻿using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using AutoBogus;
 using BMDex.Models;
 using BMDex.Resources;
 using BMDex.Services;
+using FluentAssertions;
 using Flurl.Http.Testing;
-using Newtonsoft.Json;
 using Xunit;
 
 namespace BMDex.Tests.Services
@@ -13,26 +14,29 @@ namespace BMDex.Tests.Services
     public class ResourceServiceTest
     {
         private ResourceService _resourceService;
+
         public ResourceServiceTest()
         {
             _resourceService = new ResourceService();
         }
 
         [Fact]
-        public async Task GetUrlListAsyncTest()
+        public async Task GetUrlListAsyncShouldReturnProperUrls()
         {
-            var fakeURLResult = AutoFaker.Generate<ResourceResponse>(3);
-
+            var fakeURLResult = AutoFaker.Generate<ResourceResponse>();
+            var expectedResult = fakeURLResult.Results.Select(r => r.Url);
+            
             using var httpTest = new HttpTest();
 
             httpTest
-                .ForCallsTo($"{Constants.BaseAddress}/{Endpoints.Pokemon}")
+                .ForCallsTo($"{Constants.BaseAddress}{Endpoints.Pokemon}")
                 .WithVerb(HttpMethod.Get)
-                .WithQueryParam("limit", 100)
-                .WithQueryParam("offset", 0)
-                .RespondWithJson(fakeURLResult, 200);
+                .RespondWithJson(fakeURLResult);
 
             var result = await _resourceService.GetUrlListAsync(Endpoints.Pokemon, 100, 0);
+
+            result.Should().NotBeNullOrEmpty();
+            result.Should().BeEquivalentTo(expectedResult);
         }
     }
 }
